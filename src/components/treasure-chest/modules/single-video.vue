858e771fcedac1a85fddfbd8c5770ba5d797a3e8
hexinpeng <602671134@qq.com>
2022-01-21 17:38:19 +0800

    fix(component): single-video
    修复视频自动播放功能

--- src/components/treasure-chest/modules/single-video.vue
+++ src/components/treasure-chest/modules/single-video.vue
@@ -1,29 +1,40 @@
 <template>
   <div class="treasure-chest-module single-video-container">
-    <video autoplay loop :poster="info.videoPoster" width="500">
+    <video loop :poster="info.videoPoster" width="500">
       <source :src="info.videoSrc" type="video/mp4" />
       <p>{{ info.videoAlt }}</p>
     </video>
   </div>
 </template>
-
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import { Prop } from "vue-property-decorator";
+import { Prop, Watch } from "vue-property-decorator";
 import { SingleVideo } from "@/service/model";
 
 @Component
-/** single img component */
+/** single video component */
 export default class TestA extends Vue {
   @Prop({
     default: () => ({
       videoSrc: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",
-      imgAlt: "Happy Every Day !",
+      videoAlt: "Happy Every Day !",
       videoPoster: "https://fakeimg.pl/500x500/282828/eae0d0/",
     }),
   })
   info!: SingleVideo;
+  @Watch("info", { immediate: true })
+  // eslint-disable-next-line require-jsdoc
+  videoPlay(val: string) {
+    if (val) {
+      setTimeout(() => {
+        const videoEle = document.querySelector("video");
+        if (videoEle) {
+          videoEle.play();
+        }
+      }, 0);
+    }
+  }
 }
 </script>
 
